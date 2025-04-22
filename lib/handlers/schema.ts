660ffc77eea4496e0938951c3ea65fe59f881652
duonghb53 <duonghb53@gmail.com>
2025-04-22 17:30:24 +0700

    add router address response for v3,v3s1

--- lib/handlers/schema.ts
+++ lib/handlers/schema.ts
@@ -31,6 +31,7 @@ export type V4PoolInRoute = {
 export type V3PoolInRoute = {
   type: 'v3-pool'
   address: string
+  routerAddress: string
   tokenIn: TokenInRoute
   tokenOut: TokenInRoute
   sqrtRatioX96: string
@@ -44,6 +45,7 @@ export type V3PoolInRoute = {
 export type V3S1PoolInRoute = {
   type: 'v3s1-pool'
   address: string
+  routerAddress: string
   tokenIn: TokenInRoute
   tokenOut: TokenInRoute
   sqrtRatioX96: string
