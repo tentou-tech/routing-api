2520d805524ae186cfad98caaf8363954de47b24
kien <kien@tentou.tech>
2025-05-23 11:12:52 +0000

    prettier

--- lib/handlers/quote/util/pairs-to-track.ts
+++ lib/handlers/quote/util/pairs-to-track.ts
@@ -1,13 +1,5 @@
 import { ChainId, TradeType } from '@tentou-tech/uniswap-sdk-core'
 
 export const PAIRS_TO_TRACK: Map<ChainId, Map<TradeType, string[]>> = new Map([
-  [
-    ChainId.STORY,
-    new Map([
-      [
-        TradeType.EXACT_INPUT,
-        ['IP/USDC.e','USDC.e/IP'],
-      ]
-    ]),
-  ],
+  [ChainId.STORY, new Map([[TradeType.EXACT_INPUT, ['IP/USDC.e', 'USDC.e/IP']]])],
 ])
